9fd851c8251e206dd81cf86fdd634f85dc1bd8fb
Rouxhero <leolvcdb@gmail.com>
2021-10-18 13:09:01 +0200

    Correction

--- interface/frames.py
+++ interface/frames.py
@@ -2,7 +2,6 @@ import tkinter as tk
 import tkinter.ttk as tkk
 from tkinter.filedialog import *
 from interface.entry import *
-from regex import *
 from interface.uml import *
 from interface.code import *
 from interface.data import *
